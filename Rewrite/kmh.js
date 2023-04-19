cafee94edc80c3cf91eb081e13175b303ac4316a
lixy <jackie.ky@qq.com>
2023-04-19 11:47:20 +0800

    Update kmh.js

--- Rewrite/kmh.js
+++ Rewrite/kmh.js
@@ -1,3 +1,5 @@
 var body = $response.body;
 body = body.replace(/"price":\d+/g,"\"price\":0");
+body = body.replace(/"download_price":\d+/g,"\"download_price\":0");
+body = body.replace(/"isbuy":\d+/g,"\"isbuy\":0");
 $done({body:body});
