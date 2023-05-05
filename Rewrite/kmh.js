c109d77a57b4a862ac352b5db092d936410e8a15
lixy <jackie.ky@qq.com>
2023-05-05 13:37:43 +0800

    Update kmh.js

--- Rewrite/kmh.js
+++ Rewrite/kmh.js
@@ -2,4 +2,5 @@ var body = $response.body;
 body = body.replace(/"price":\d+/g,"\"price\":0");
 body = body.replace(/"download_price":\d+/g,"\"download_price\":0");
 body = body.replace(/"isbuy":\d+/g,"\"isbuy\":0");
+body = body.replace(/"level_detail":\d+/g,"\"level_detail\":2945569034000");
 $done({body:body});
