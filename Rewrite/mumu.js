db519345571ae8506d2fd5cc058d5b4c0124119a
keepyounger <jackie.ky@qq.com>
2024-05-07 17:53:59 +0800

    mumu

--- Rewrite/mumu.js
+++ Rewrite/mumu.js
@@ -1,14 +1,5 @@
 var body = $response.body;
 body = {
-    "items" : [
-        {
-            "size" : 422646441,
-            "releaseNote" : "- 修复了部分已知问题。",
-            "downloadLink" : "https://a11.gdl.netease.com/MuMuPlayerPro-v1.4.11.tar.gz",
-            "checksum" : "eda0f710f01ab133a1203cbb7353217c",
-            "name" : "player",
-            "version" : "1.3.6"
-        }
-    ]
+    "items" : []
 }
 $done({body:body});
--- Rewrite/mumu3.js
+++ Rewrite/mumu3.js
@@ -1,3 +1,21 @@
 var body = $response.body;
-body = {}
+body = {
+    "code": 0,
+    "msg": "ok",
+    "data": {
+        "user_id": "1",
+        "nickname": "xx",
+        "member_expired_at": 1,
+        "member_status": 1,
+        "enabled_device_count": 1,
+        "current_device_status": 1,
+        "current_device": {
+            "device_id": "1",
+            "alias": "MacBook Pro",
+            "last_binded_at": 1710413040,
+            "trial_end_at": 1710950399,
+            "trial_status": 1
+        }
+    }
+}
 $done({body:body});
