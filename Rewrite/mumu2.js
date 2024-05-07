e77cb87539399c121c3365a02a955676c45507a8
keepyounger <jackie.ky@qq.com>
2024-05-07 17:42:49 +0800

    mumu

---
+++ Rewrite/mumu2.js
@@ -0,0 +1,13 @@
+var body = $response.body;
+body = {
+    "msg" : "ok",
+    "data" : {
+        "trial_status" : 0,
+        "alias" : "lixy的MacBook Pro",
+        "device_id" : "aeawlzvg6eaabd4e",
+        "last_binded_at" : 0,
+        "trial_end_at" : 0
+    },
+    "code" : 0
+}
+$done({body:body});
